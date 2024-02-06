12c8389ea528146eaddb43d2c7b18f73e43b4067
Jetistrim <paulogabriellima@hotmail.com>
2024-02-06 12:00:26 -0300

    inicio do pagode

---
+++ Js/index.js
@@ -0,0 +1,29 @@
+mes.onchange = () =>{
+    switch (mes) {
+        case "Janeiro":
+            nome.innerHTML = "Rei"
+            break
+        case "Fevereiro":
+            break
+        case "Janeiro":
+            break
+        case "Janeiro":
+            break
+        case "Janeiro":
+            break
+        case "Janeiro":
+            break
+        case "Janeiro":
+            break
+        case "Janeiro":
+            break
+        case "Janeiro":
+            break
+        case "Janeiro":
+            break
+        case "Janeiro":
+            break
+        case "Janeiro":
+            break
+    }
+}
