fe3375b94757b3ca72befef4dd3deea634931018
Paulo Gabriel Lima de Sousa <paulogabriellima@hotmail.com>
2024-02-08 00:02:06 -0300

    completando o js - desfuncional

--- Js/index.js
+++ Js/index.js
@@ -1,29 +1,77 @@
-mes.onchange = () =>{
+mes.onchange = () => {
     switch (mes) {
         case "Janeiro":
             nome.innerHTML = "Rei"
             break
         case "Fevereiro":
-            break
-        case "Janeiro":
-            break
-        case "Janeiro":
-            break
-        case "Janeiro":
-            break
-        case "Janeiro":
-            break
-        case "Janeiro":
-            break
-        case "Janeiro":
-            break
-        case "Janeiro":
-            break
-        case "Janeiro":
-            break
-        case "Janeiro":
-            break
-        case "Janeiro":
+            nome.innerHTML = "Mô"
             break
     }
 }
+//         case "Março":
+//             nome.innerHTML = "Cupido"
+//             break
+//         case "Abril":
+//             nome.innerHTML = "Apaixonado"
+//             break
+//         case "Maio":
+//             nome.innerHTML = "Pagodeiro"
+//             break
+//         case "Junho":
+//             nome.innerHTML = "Cara Perfeito"
+//             break
+//         case "Julho":
+//             nome.innerHTML = "Mensageiro"
+//             break
+//         case "Agosto":
+//             nome.innerHTML = "Pertubado"
+//             break
+//         case "Setembro":
+//             nome.innerHTML = "Gueri Gueri"
+//             break
+//         case "Outubro":
+//             nome.innerHTML = "Coração"
+//             break
+//         case "Novembro":
+//             nome.innerHTML = "Linda Voz"
+//             break
+//         case "Dezembro":
+//             nome.innerHTML = "Criado Mudo"
+//             break
+//     }
+// };
+
+// dia.onchange = () => {
+//     switch (dia) {
+//         case 1:
+//             nome.innerHTML = "Que Tanto Fez ou Tanto Faz"
+//             break
+//         case 4:
+//             nome.innerHTML = "Que Desabou o Barraco"
+//             break
+//         case 7:
+//             nome.innerHTML = "Que Largou o Freio"
+//             break
+//         case 10:
+//             nome.innerHTML = "Que Quer Futicar"
+//             break
+//         case 13:
+//             nome.innerHTML = "Que Tem Fogo Na Roupa"
+//             break
+//         case 16:
+//             nome.innerHTML = "Que Se Apaixonou Pela Pessoa Errada"
+//             break
+//         case 19:
+//             nome.innerHTML = "Que Desliga E Vem Depressa"
+//             break
+//         case 22:
+//             nome.innerHTML = "Que Está À Deriva"
+//             break
+//         case 25:
+//             nome.innerHTML = "Que Segura O Samba"
+//             break
+//         case 28:
+//             nome.innerHTML = "Que Ninguém Ama"
+//             break
+//     }
+// }
