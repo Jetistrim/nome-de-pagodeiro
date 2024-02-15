3f9a59db46a794783f77f1fd2b0773561efda3a2
Jetistrim <paulogabriellima@hotmail.com>
2024-02-15 10:14:12 -0300

    correções finais

--- Js/index.js
+++ Js/index.js
@@ -1,77 +1,144 @@
+for(let i = 1; i <= 31; i++){
+    dia.innerHTML += `<option>${i}</option>`
+}
+
 mes.onchange = () => {
-    switch (mes) {
+    switch (mes.value) {
         case "Janeiro":
-            nome.innerHTML = "Rei"
-            break
+            nomeDePagodeiro.innerHTML = "Rei"
+            break;
         case "Fevereiro":
-            nome.innerHTML = "Mô"
+            nomeDePagodeiro.innerHTML = "Mô"
+            break;
+        case "Março":
+            nomeDePagodeiro.innerHTML = "Cupido "
+            break
+        case "Abril":
+            nomeDePagodeiro.innerHTML = "Apaixonado "
+            break
+        case "Maio":
+            nomeDePagodeiro.innerHTML = "Pagodeiro "
+            break
+        case "Junho":
+            nomeDePagodeiro.innerHTML = "Cara Perfeito "
+            break
+        case "Julho":
+            nomeDePagodeiro.innerHTML = "Mensageiro "
+            break
+        case "Agosto":
+            nomeDePagodeiro.innerHTML = "Pertubado "
+            break
+        case "Setembro":
+            nomeDePagodeiro.innerHTML = "Gueri Gueri "
+            break
+        case "Outubro":
+            nomeDePagodeiro.innerHTML = "Coração "
+            break
+        case "Novembro":
+            nomeDePagodeiro.innerHTML = "Linda Voz "
+            break
+        case "Dezembro":
+            nomeDePagodeiro.innerHTML = "Criado Mudo "
             break
     }
-}
-//         case "Março":
-//             nome.innerHTML = "Cupido"
-//             break
-//         case "Abril":
-//             nome.innerHTML = "Apaixonado"
-//             break
-//         case "Maio":
-//             nome.innerHTML = "Pagodeiro"
-//             break
-//         case "Junho":
-//             nome.innerHTML = "Cara Perfeito"
-//             break
-//         case "Julho":
-//             nome.innerHTML = "Mensageiro"
-//             break
-//         case "Agosto":
-//             nome.innerHTML = "Pertubado"
-//             break
-//         case "Setembro":
-//             nome.innerHTML = "Gueri Gueri"
-//             break
-//         case "Outubro":
-//             nome.innerHTML = "Coração"
-//             break
-//         case "Novembro":
-//             nome.innerHTML = "Linda Voz"
-//             break
-//         case "Dezembro":
-//             nome.innerHTML = "Criado Mudo"
-//             break
-//     }
-// };
+};
 
-// dia.onchange = () => {
-//     switch (dia) {
-//         case 1:
-//             nome.innerHTML = "Que Tanto Fez ou Tanto Faz"
-//             break
-//         case 4:
-//             nome.innerHTML = "Que Desabou o Barraco"
-//             break
-//         case 7:
-//             nome.innerHTML = "Que Largou o Freio"
-//             break
-//         case 10:
-//             nome.innerHTML = "Que Quer Futicar"
-//             break
-//         case 13:
-//             nome.innerHTML = "Que Tem Fogo Na Roupa"
-//             break
-//         case 16:
-//             nome.innerHTML = "Que Se Apaixonou Pela Pessoa Errada"
-//             break
-//         case 19:
-//             nome.innerHTML = "Que Desliga E Vem Depressa"
-//             break
-//         case 22:
-//             nome.innerHTML = "Que Está À Deriva"
-//             break
-//         case 25:
-//             nome.innerHTML = "Que Segura O Samba"
-//             break
-//         case 28:
-//             nome.innerHTML = "Que Ninguém Ama"
-//             break
-//     }
-// }
+dia.onchange = () => {
+    switch (Number(dia.value)) {
+        case 1:
+            nomeDePagodeiro.innerHTML += "Que Tanto Fez ou Tanto Faz"
+            break;
+        case 2:
+            nomeDePagodeiro.innerHTML += "Que Tanto Fez ou Tanto Faz"
+            break;
+        // case 3:
+        //     nomeDePagodeiro.innerHTML += "Que Tanto Fez ou Tanto Faz"
+        //     break;
+        // case 4:
+        //     nomeDePagodeiro.innerHTML += "Que Desabou o Barraco"
+        //     break;
+        // case 5:
+        //     nomeDePagodeiro.innerHTML += "Que Desabou o Barraco"
+        //     break;
+        // case 6:
+        //     nomeDePagodeiro.innerHTML += "Que Desabou o Barraco"
+        //     break;
+        // case 7:
+        //     nomeDePagodeiro.innerHTML += "Que Largou o Freio"
+        //     break;
+        // case 8:
+        //     nomeDePagodeiro.innerHTML += "Que Largou o Freio"
+        //     break;
+        // case 9:
+        //     nomeDePagodeiro.innerHTML += "Que Largou o Freio"
+        //     break;
+        // case 10:
+        //     nomeDePagodeiro.innerHTML += "Que Quer Futicar"
+        //     break;
+        // case 11:
+        //     nomeDePagodeiro.innerHTML += "Que Quer Futicar"
+        //     break;
+        // case 12:
+        //     nomeDePagodeiro.innerHTML += "Que Quer Futicar"
+        //     break;
+        // case 13:
+        //     nomeDePagodeiro.innerHTML += "Que Tem Fogo Na Roupa"
+        //     break;
+        // case 14:
+        //     nomeDePagodeiro.innerHTML += "Que Tem Fogo Na Roupa"
+        //     break;
+        // case 15:
+        //     nomeDePagodeiro.innerHTML += "Que Tem Fogo Na Roupa"
+        //     break;
+        // case 16:
+        //     nomeDePagodeiro.innerHTML += "Que Se Apaixonou Pela Pessoa Errada"
+        //     break;
+        // case 17:
+        //     nomeDePagodeiro.innerHTML += "Que Se Apaixonou Pela Pessoa Errada"
+        //     break;
+        // case 18:
+        //     nomeDePagodeiro.innerHTML += "Que Se Apaixonou Pela Pessoa Errada"
+        //     break;
+        // case 19:
+        //     nomeDePagodeiro.innerHTML += "Que Desliga E Vem Depressa"
+        //     break;
+        // case 20:
+        //     nomeDePagodeiro.innerHTML += "Que Desliga E Vem Depressa"
+        //     break;
+        // case 21:
+        //     nomeDePagodeiro.innerHTML += "Que Desliga E Vem Depressa"
+        //     break;
+        // case 22:
+        //     nomeDePagodeiro.innerHTML += "Que Está À Deriva"
+        //     break;
+        // case 23:
+        //     nomeDePagodeiro.innerHTML += "Que Está À Deriva"
+        //     break;
+        // case 24:
+        //     nomeDePagodeiro.innerHTML += "Que Está À Deriva"
+        //     break;
+        // case 25:
+        //     nomeDePagodeiro.innerHTML += "Que Segura O Samba"
+        //     break;
+        // case 26:
+        //     nomeDePagodeiro.innerHTML += "Que Segura O Samba"
+        //     break;
+        // case 27:
+        //     nomeDePagodeiro.innerHTML += "Que Segura O Samba"
+        //     break;
+        // case 28:
+        //     nomeDePagodeiro.innerHTML += "Que Ninguém Ama"
+        //     break;
+        // case 29:
+        //     nomeDePagodeiro.innerHTML += "Que Ninguém Ama"
+        //     break;
+        // case 30:
+        //     nomeDePagodeiro.innerHTML += "Que Ninguém Ama"
+        //     break;
+        // case 31:
+        //     nomeDePagodeiro.innerHTML += "Que Ninguém Ama"
+        //     break;
+        default:
+            nomeDePagodeiro.innerHTML= "Error"
+    }
+}
